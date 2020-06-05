8c03e2f2e6762d7c49ce2bef70b8d26ac1844355
Bryan Larson <b32147@gmail.com>
2020-06-05 11:38:09 -0400

    Updated 3.x.x code for Dstu2/Dstu3/R4 deployments; consolidated docker-compose.yml

--- hapi-fhir-3.x.x/src/main/java/hms/dbmi/ppm/FhirServerConfigDstu3.java
+++ hapi-fhir-3.x.x/src/main/java/hms/dbmi/ppm/FhirServerConfigDstu3.java
@@ -35,7 +35,7 @@ public class FhirServerConfigDstu3 extends BaseJavaConfigDstu3 {
     @Bean()
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean retVal = super.entityManagerFactory();
-        retVal.setPersistenceUnitName("HAPI_PU");
+        retVal.setPersistenceUnitName(HapiProperties.getPersistenceUnitName());
 
         try {
             retVal.setDataSource(myDataSource);
@@ -43,7 +43,7 @@ public class FhirServerConfigDstu3 extends BaseJavaConfigDstu3 {
             throw new ConfigurationException("Could not set the data source due to a configuration issue", e);
         }
 
-        retVal.setJpaProperties(HapiProperties.getProperties());
+        retVal.setJpaProperties(HapiProperties.getJpaProperties());
         return retVal;
     }
 
--- hapi-fhir-3.x.x/src/main/java/hms/dbmi/ppm/FhirServerConfigR4.java
+++ hapi-fhir-3.x.x/src/main/java/hms/dbmi/ppm/FhirServerConfigR4.java
@@ -35,7 +35,7 @@ public class FhirServerConfigR4 extends BaseJavaConfigR4 {
     @Bean()
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean retVal = super.entityManagerFactory();
-        retVal.setPersistenceUnitName("HAPI_PU");
+        retVal.setPersistenceUnitName(HapiProperties.getPersistenceUnitName());
 
         try {
             retVal.setDataSource(myDataSource);
@@ -43,7 +43,7 @@ public class FhirServerConfigR4 extends BaseJavaConfigR4 {
             throw new ConfigurationException("Could not set the data source due to a configuration issue", e);
         }
 
-        retVal.setJpaProperties(HapiProperties.getProperties());
+        retVal.setJpaProperties(HapiProperties.getJpaProperties());
         return retVal;
     }
 
